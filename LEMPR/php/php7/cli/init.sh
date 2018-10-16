51b52f092ae9b577a6273199fe970be6d2edcca7
刘鎏 <liuliu@952.com>
2018-10-16 11:34:32 +0800

    Changes to the way logs written in the cli container

--- LEMPR/php/php7/cli/init.sh
+++ LEMPR/php/php7/cli/init.sh
@@ -5,5 +5,5 @@ USER="www-data"
 # CLI Command
 # Example：task queue of laravel
 # cd $DATA
-# su-exec $USER php artisan queue:work redis --queue=uc:sms:listener --env=$APP_ENV >/logs/sms.log 2>&1 &
-# su-exec $USER php artisan queue:work redis --queue=uc:email:listener --env=$APP_ENV >/logs/email.log 2>&1
+# su-exec $USER php artisan queue:work redis --queue=uc:sms:listener --env=$APP_ENV >>/logs/sms.log 2>&1 &
+# su-exec $USER php artisan queue:work redis --queue=uc:email:listener --env=$APP_ENV >>/logs/email.log 2>&1
